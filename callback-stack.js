bf5eb6b4d3a36cd72a5cc97f9d436cb793fa20ba
Le Anh Quan <quanla2003@gmail.com>
2021-04-21 14:58:40 +0700

    1.0.1

--- callback-stack.js
+++ callback-stack.js
@@ -7,7 +7,10 @@ const cbs1 = (services, stack) => {
     const [first, ...last] = services;
     const [name, fn] = Array.isArray(first) ? first : [null, first];
 
-    return fn(stack, !last.length ? null : (
-        (ret) => cbs1(last, name != null ? {...stack, [name]: ret} : stack)
-    ));
+    return fn(
+        stack,
+        !last.length ? null : (
+            (ret) => cbs1(last, name != null ? {...stack, [name]: ret} : stack)
+        )
+    );
 };
